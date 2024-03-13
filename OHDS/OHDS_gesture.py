2c4104474a2dc8346b0c76f9b8d8eb3bbd1c253f
christophergyman <christophergayiuman@gmail.com>
2024-03-13 21:07:09 +0000

    added hand gesture recognition

--- OHDS/OHDS_gesture.py
+++ OHDS/OHDS_gesture.py
@@ -1,6 +1,14 @@
 import cv2
 import mediapipe as mp
 
+def Print_Finger_Loc(thumb_tip, index_finger_tip, middle_finger_tip, ring_finger_tip, little_finger_tip):
+    print("Thumb tip:", thumb_tip)
+    print("Index finger tip:", index_finger_tip)
+    print("Middle finger tip:", middle_finger_tip)
+    print("Ring finger tip:", ring_finger_tip)
+    print("Little finger tip:", little_finger_tip)
+
+
 # Initialize MediaPipe Hands.
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.7)
@@ -25,15 +33,16 @@ while cap.isOpened():
             # Draw hand landmarks on the image.
             mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-            # Get thumb and index finger landmarks.
+            # Store positions of all finger landmarks into variables.
             thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
             index_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+            middle_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
+            ring_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
+            little_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP]
 
-            # Check if thumb tip is above index finger tip.
-            if thumb_tip.y < index_finger_tip.y:
-                print("Thumbs up!")
-            else:
-                print("nothing detected")
+            # Print positions of finger landmarks.
+            Finger_Position_Array = []
+            Finger_Position_Array.append
 
     # Display the image.
     cv2.imshow('Hand Gesture Recognition', image)
